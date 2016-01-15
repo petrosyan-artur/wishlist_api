9ba1d56b9581f92aa67bd4af77bf4086fbcf3fe3
Aram Knyazyan <aramknyazyan@gmail.com>
2016-01-15 20:09:28 +0400

    --adminpage usersearch issue is fixed

--- public/app/controllers/privateCtrl.js
+++ public/app/controllers/privateCtrl.js
@@ -66,6 +66,7 @@ angular.module('privateCtrl', ['privateService','userService', 'wishService'])
         vm.findUser = function() {
 
             if (vm.user === undefined || !vm.user.username || vm.user.username == '') {
+                console.log(vm.user.username);
                 alert('Username is empty!');
                 return false;
             }
